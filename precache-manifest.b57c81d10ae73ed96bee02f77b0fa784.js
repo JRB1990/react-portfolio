587627a8da2813a86f68962060998bb48dfdecf1
Josh B <barber5702@hotmail.com>
2020-10-07 11:06:43 -0700

    Updates

---
+++ precache-manifest.b57c81d10ae73ed96bee02f77b0fa784.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd2b675b2b045513e15657e6dc0ed804",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3a8bbf848eadc156866",
+    "url": "/static/css/2.8d976990.chunk.css"
+  },
+  {
+    "revision": "4f9e3f10dc31748c36f3",
+    "url": "/static/css/main.d1cbbad6.chunk.css"
+  },
+  {
+    "revision": "e3a8bbf848eadc156866",
+    "url": "/static/js/2.9b649dce.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9b649dce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f9e3f10dc31748c36f3",
+    "url": "/static/js/main.c813aa76.chunk.js"
+  },
+  {
+    "revision": "d141e070661a418ffe54",
+    "url": "/static/js/runtime-main.69c967f3.js"
+  },
+  {
+    "revision": "cbec739d5852f377691ccd1b3efccbec",
+    "url": "/static/media/Josh_Profile_Pic.cbec739d.png"
+  },
+  {
+    "revision": "d6bd8ff187c4e2d3a341c526e0836097",
+    "url": "/static/media/background.d6bd8ff1.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.67507f90b7954c0d2d27b5fc1645687c.js"
+  "/precache-manifest.b57c81d10ae73ed96bee02f77b0fa784.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
